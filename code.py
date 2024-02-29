fa91fff2cf39dfbe7cd72f4959e96585999d354e
Devvrat07 <150048765+Devvrat07@users.noreply.github.com>
2024-03-01 00:07:35 +0530

    async_def.mainpy

--- code.py
+++ code.py
@@ -50,15 +50,12 @@ async def main_loop():
     global led,button1
 
     button_task = asyncio.create_task(monitor_buttons(button1))
-    if(board.board_id == 'raspberry_pi_pico_w'):
-        pico_led_task = asyncio.create_task(blink_pico_w_led(led))
+    (board.board_id == 'raspberry_pi_pico_w'):
+    pico_led_task = asyncio.create_task(blink_pico_w_led(led))
        
-        startWiFi()
+    startWiFi()
         
-        webservice_task = asyncio.create_task(startWebService())
-        await asyncio.gather(pico_led_task, button_task, webservice_task)
-    else:
-        pico_led_task = asyncio.create_task(blink_pico_led(led))
-        await asyncio.gather(pico_led_task, button_task)
-
+    webservice_task = asyncio.create_task(startWebService())
+    await asyncio.gather(pico_led_task, button_task, webservice_task)
+  
 asyncio.run(main_loop())
