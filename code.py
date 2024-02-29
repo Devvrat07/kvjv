57f74de9ee7e315a358589d4041f13807288c0fe
Devvrat07 <150048765+Devvrat07@users.noreply.github.com>
2024-02-29 23:18:05 +0530

    Update code.py

--- code.py
+++ code.py
@@ -11,29 +11,23 @@ if(board.board_id == 'raspberry_pi_pico_w'):
     import wifi
     from webapp import *
 
-
-# sleep at the start to allow the device to be recognized by the host computer
 time.sleep(.5)
 
 def startWiFi():
     import ipaddress
-    # Get wifi details and more from a secrets.py file
+    
     try:
         from wificonfig import wifi
     except ImportError:
-        print("WiFi secrets are kept in secrets.py, please add them there!")
         raise
 
-    print("Connect wifi")
-    #wifi.radio.connect(secrets['ssid'],secrets['password'])
     wifi.radio.start_ap(wificonfig['id'],wificonfig['password'])
 
     HOST = repr(wifi.radio.ipv4_address_ap)
-    PORT = 80        # Port to listen on
+    PORT = 80       
     print(HOST,PORT)
 
-# turn off automatically reloading when files are written to the pico
-#supervisor.disable_autoreload()
+
 supervisor.runtime.autoreload = False
 
 if(board.board_id == 'raspberry_pi_pico'):
@@ -45,17 +39,14 @@ elif(board.board_id == 'raspberry_pi_pico_w'):
 
 progStatus = False
 progStatus = getProgrammingStatus()
-print("progStatus", progStatus)
 if(progStatus == False):
-    print("Finding payload")
-    # not in setup mode, inject the payload
     payload = selectPayload()
-    print("Running ", payload)
+    
     runScript(payload)
 
-    print("Done")
+    
 else:
-    print("Update your payload")
+    print(" ")
 
 led_state = False
 
@@ -65,9 +56,7 @@ async def main_loop():
     button_task = asyncio.create_task(monitor_buttons(button1))
     if(board.board_id == 'raspberry_pi_pico_w'):
         pico_led_task = asyncio.create_task(blink_pico_w_led(led))
-        print("Starting Wifi")
         startWiFi()
-        print("Starting Web Service")
         webservice_task = asyncio.create_task(startWebService())
         await asyncio.gather(pico_led_task, button_task, webservice_task)
     else:
