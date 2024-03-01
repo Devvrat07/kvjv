0134a066b169b46e24c14f4a647c90c15121dfdc
Regular-platypus <118298840+Regular-platypus@users.noreply.github.com>
2024-03-01 10:35:22 +0530

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,8 +1,1 @@
-from board import *
-import board
-import digitalio
-import storage
 
-noStoragePin = digitalio.DigitalInOut(GP15)
-noStoragePin.switch_to_input(pull=digitalio.Pull.UP)
-noStorage = noStoragePin.value
