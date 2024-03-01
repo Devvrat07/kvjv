29254598e7cc85ac50ca007a4ab996522d390163
Devvrat07 <150048765+Devvrat07@users.noreply.github.com>
2024-03-01 11:15:47 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,4 @@
-
 import supervisor
-
-
 import time
 import digitalio
 from board import *
@@ -9,33 +6,21 @@ import board
 from duckyinpython import *
 import wifi
 from webapp import *
-
-
-
 time.sleep(.5)
 
 def startWiFi():
     import ipaddress
-  
     try:
         from secrets import secrets
     except ImportError:
         raise
-
-    
     wifi.radio.start_ap(secrets['ssid'],secrets['password'])
-
     HOST = repr(wifi.radio.ipv4_address_ap)
     PORT = 80       
    
-
-
 supervisor.runtime.autoreload = False
-
-
 led = digitalio.DigitalInOut(board.LED)
 led.switch_to_output()
-
 
 progStatus = False
 progStatus = getProgrammingStatus()
@@ -45,7 +30,6 @@ if(progStatus == False):
     payload = selectPayload()
     print("Running ", payload)
     runScript(payload)
-
     print("Done")
 else:
     print("Update your payload")
@@ -54,9 +38,7 @@ led_state = False
 
 async def main_loop():
     global led,button1
-
     button_task = asyncio.create_task(monitor_buttons(button1))
-    
     pico_led_task = asyncio.create_task(blink_pico_w_led(led))
     startWiFi()
     webservice_task = asyncio.create_task(startWebService())
